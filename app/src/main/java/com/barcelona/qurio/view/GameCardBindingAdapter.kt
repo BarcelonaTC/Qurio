b64fac9bd58759b59957474c1b2a4474e542410b
nada <nadamansoursamir@gmail.com>
2025-10-08 01:11:33 +0300

    feat: create GameCardBindingAdapter

---
+++ app/src/main/java/com/barcelona/qurio/view/GameCardBindingAdapter.kt
@@ -0,0 +1,26 @@
+package com.barcelona.qurio.view
+
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
+import com.barcelona.qurio.customView.GameCardCustomView
+import com.barcelona.qurio.domain.entity.GameCard
+
+@BindingAdapter("app:bindCardData")
+fun bindGameCardData(view: GameCardCustomView, data: GameCard) {
+
+    val colorResId = data.cardBorderColor
+    val actualColor = ContextCompat.getColor(view.context, colorResId)
+    view.setCardBorderColor(actualColor)
+
+    val gradientColorResId = data.cardBottomGradient
+    val gradientDrawable = ContextCompat.getDrawable(view.context, gradientColorResId)
+    view.setCardBottomGradientDrawable(gradientDrawable)
+
+    val strokeWidth=data.cardBorderWidth
+    view.setCardBorderWidth(strokeWidth.toFloat())
+
+    val imageResId = data.cardImage
+    view.setCardSrc(imageResId)
+
+
+}
