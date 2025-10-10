67de0786aff53dc22cbb4e2f727539985bae0c99
Elsayed Magdy <saikoo1239@gmail.com>
2025-10-10 16:15:07 +0300

    feat: Add trivia game and stats repositories

---
+++ app/src/main/java/com/barcelona/qurio/model/repository/TriviaGameRepositoryImpl.kt
@@ -0,0 +1,28 @@
+package com.barcelona.qurio.model.repository
+
+import com.barcelona.qurio.model.api.TriviaApiService
+import com.barcelona.qurio.model.dto.toDomain
+import com.barcelona.qurio.presentation.model.Question
+import javax.inject.Inject
+
+class TriviaGameRepositoryImpl @Inject constructor(
+    private val triviaApiService: TriviaApiService
+) : TriviaGameRepository {
+    override suspend fun fetchQuestions(
+        amount: Int,
+        difficulty: String,
+        type: String
+    ): List<Question> {
+        return try {
+            triviaApiService.getQuestions(
+                amount = amount,
+                difficulty = difficulty,
+                type = type
+            ).results
+                ?.mapNotNull { it?.toDomain() }
+                .orEmpty()
+        } catch (e: Exception) {
+            emptyList()
+        }
+    }
+}
