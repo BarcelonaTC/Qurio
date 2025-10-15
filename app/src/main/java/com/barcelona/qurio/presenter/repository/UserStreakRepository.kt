3c3246fb22541c6f86c87b989df391c72042e722
Elsayed Magdy <saikoo1239@gmail.com>
2025-10-15 15:22:53 +0300

    feat: Add lives system for user streak

--- app/src/main/java/com/barcelona/qurio/presenter/repository/UserStreakRepository.kt
+++ app/src/main/java/com/barcelona/qurio/presenter/repository/UserStreakRepository.kt
@@ -5,4 +5,7 @@ import com.barcelona.qurio.presentation.model.streak.StreakModel
 interface UserStreakRepository {
     suspend fun updateStreak()
     suspend fun getStreakInfo(): StreakModel
+    suspend fun getLivesCount(): Int
+    suspend fun incrementLives()
+    suspend fun decrementLives()
 }
