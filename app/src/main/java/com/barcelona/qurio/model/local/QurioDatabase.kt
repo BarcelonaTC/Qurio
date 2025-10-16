b1113da6d707d54c325f7a9869f8c6d07c7930bb
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-10-16 19:38:45 +0300

    feature: add volume table and it's dao to the database

--- app/src/main/java/com/barcelona/qurio/model/local/QurioDatabase.kt
+++ app/src/main/java/com/barcelona/qurio/model/local/QurioDatabase.kt
@@ -5,10 +5,16 @@ import androidx.room.RoomDatabase
 import com.barcelona.qurio.model.local.dao.GameSessionDao
 import com.barcelona.qurio.model.local.entity.TriviaGameSessionEntity
 import com.barcelona.qurio.model.local.dao.UserStreakDao
+import com.barcelona.qurio.model.local.dao.VolumeLevelDao
 import com.barcelona.qurio.model.local.entity.UserStreakEntity
+import com.barcelona.qurio.model.local.entity.VolumeEntity
 
-@Database(entities = [TriviaGameSessionEntity::class, UserStreakEntity::class], version = 1)
+@Database(
+    entities = [TriviaGameSessionEntity::class, UserStreakEntity::class, VolumeEntity::class],
+    version = 1
+)
 abstract class QurioDatabase : RoomDatabase() {
     abstract fun gameSessionDao(): GameSessionDao
     abstract fun userStreakDao(): UserStreakDao
+    abstract fun volumeLevelDao(): VolumeLevelDao
 }
